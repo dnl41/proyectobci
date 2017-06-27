432a714686397ca5f93c25e69fbc4178f6b7a571
Daniel Urgiles <daniel-urg@hotmail.com>
2017-06-26 19:31:04 -0500

    730

--- src/app/grafica-frecuencia/grafica-frecuencia.component.ts
+++ src/app/grafica-frecuencia/grafica-frecuencia.component.ts
@@ -10,11 +10,55 @@ import * as io from 'socket.io-client';
   templateUrl: './grafica-frecuencia.component.html',
   styleUrls: ['./grafica-frecuencia.component.css']
 })
-export class GraficaFrecuenciaComponent implements OnInit {
+export class GraficaFrecuenciaComponent implements OnInit, OnDestroy {
+   socket: any;
+  constructor(private view: ElementRef,
+  	          private chartService: ChartService) { 
+      this.socket = io('http://localhost:8080');
+     this.chartService = chartService;}
 
-  constructor(private chartService: ChartService) { }
+  private options = this.chartService.getChartSmoothieDefaults();
+  private channels = this.chartService.getChannels();
+  private colors = this.chartService.getColors();
+  private timeSeries = new SmoothieChart(this.options);
+  private amplitudes = [];
+  private timeline = [];
+  private lines = Array(8).fill(0).map(() => new TimeSeries());
 
-  ngOnInit() {
+ ngOnInit() {
+    this.addTimeSeriesLines();
+        
+    this.socket.on('bci:time', (data) => {
+      this.amplitudes = data.amplitudes;
+      this.timeline = data.timeline;
+      this.appendTimeSeriesLines(data.data);
+    });
+  }
+  
+  ngOnDestroy () {
+    this.socket.removeListener('bci:time');
+  } 
+  
+  ngAfterViewInit () {
+    this.timeSeries.streamTo(
+      this.view.nativeElement.querySelector('#timeSeries')
+    );
+  }
+  
+  addTimeSeriesLines () {
+    this.lines.forEach((line, index) => {
+        this.timeSeries.addTimeSeries(line, { 
+          strokeStyle: this.colors[index].borderColor 
+        });
+    });
+  }
+  
+  appendTimeSeriesLines (data) {
+    this.lines.forEach((line, index) => {
+          data[index].forEach((amplitude) => {
+              line.append(new Date().getTime(), amplitude);
+          });
+      });
   }
 
 }
